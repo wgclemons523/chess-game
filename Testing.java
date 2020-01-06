937923b2aa536d055e445a0fa04a81dbe81c9343
wgclemons523 <59568391+wgclemons523@users.noreply.github.com>
2020-01-06 09:48:40 -0500

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,13 @@
+package com.chess;
+
+
+public class Testing {
+
+	public static void main(String[] args) {
+		String defaultPieceImagesPath = "C:/Users/clemowg/Desktop/chessImages/BB.gif";
+		
+		System.out.println(defaultPieceImagesPath);
+
+	}
+
+}
